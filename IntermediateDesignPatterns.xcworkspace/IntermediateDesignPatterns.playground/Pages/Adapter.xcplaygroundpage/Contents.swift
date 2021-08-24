5c82885665daaa8d218e4cb07440b785b77f46b2
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-08-24 22:37:13 +0530

    Included Factory Design Pattern

--- IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
+++ IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Adapter.xcplaygroundpage/Contents.swift
@@ -17,3 +17,4 @@
  
  ## Code Example
  */
+
--- IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
+++ IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
@@ -12,3 +12,64 @@
  
  ## Code Example
  */
+
+import Foundation
+
+public struct JobApplicant {
+    public let name: String
+    public let email: String
+    public var status: Status
+
+    public enum Status {
+        case new
+        case interview
+        case hired
+        case rejected
+    }
+}
+
+public struct Email{
+    public let subject: String
+    public let senderEmail: String
+    public let recipientEmail: String
+    public let messageBody: String
+}
+
+public struct EmailFactory {
+    public let senderEmail: String
+
+    public func createEmail(to recipient: JobApplicant) -> Email {
+        let subject: String
+        let messageBody: String
+
+        switch recipient.status {
+        case .new:
+            subject = "We received your Application"
+            messageBody = "Thanks for applying Job here"
+        case .interview:
+            subject = "We want to take your interview"
+            messageBody = "Congratulations \(recipient.name), we impressed by your Resume, and wanna schedule your interview"
+        case .hired:
+            subject = "We want to hire you"
+            messageBody = "Congratulations \(recipient.name), we like to work with you"
+        case .rejected:
+            subject = "Thanks for your application"
+            messageBody = "Sorry to say that, you didn't get seleced, you can apply again after 6 months, best of luck next time"
+        }
+
+        return Email(subject: subject, senderEmail: senderEmail, recipientEmail: recipient.email, messageBody: messageBody)
+    }
+}
+
+var bhavesh = JobApplicant(name: "Bhavesh Gupta", email: "bhaveshgutpa296@gmail.com", status: .new)
+
+let emailFactory = EmailFactory(senderEmail: "hr@careers.com")
+
+print(emailFactory.createEmail(to: bhavesh))
+
+bhavesh.status = .interview
+
+print(emailFactory.createEmail(to: bhavesh))
+
+bhavesh.status = .hired
+print(emailFactory.createEmail(to: bhavesh))
