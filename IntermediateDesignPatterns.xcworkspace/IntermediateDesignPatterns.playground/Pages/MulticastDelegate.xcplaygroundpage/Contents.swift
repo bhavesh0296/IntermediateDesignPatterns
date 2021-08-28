3d11c481a4a5a5755f53e9e9fabda2a14b7fc076
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-08-28 15:00:07 +0530

    included MultiCast Delegate Design pattern

--- IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/MulticastDelegate.xcplaygroundpage/Contents.swift
+++ IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/MulticastDelegate.xcplaygroundpage/Contents.swift
@@ -13,3 +13,91 @@
  
  ## Code Example
  */
+import UIKit
+
+public class MulticastDelegate<ProtocolType> {
+
+    private class DelegateWrapper {
+        weak var delegate: AnyObject?
+
+        init(_ delegate: AnyObject) {
+            self.delegate = delegate
+        }
+    }
+
+    private var delegateWrappers: [DelegateWrapper]
+
+    public var delegates: [ProtocolType] {
+        delegateWrappers = delegateWrappers.filter { $0.delegate != nil }
+        return delegateWrappers.map { $0.delegate } as! [ProtocolType]
+    }
+
+    public init(_ delegates: [ProtocolType] = []){
+        delegateWrappers = delegates.map { DelegateWrapper($0 as AnyObject)}
+    }
+
+    public func addDelegate(_ delegate: ProtocolType) {
+        delegateWrappers.append(DelegateWrapper(delegate as AnyObject))
+    }
+
+    public func removeDelegate(_ delegate: ProtocolType){
+        guard let index = delegateWrappers.firstIndex(where: { $0.delegate === (delegate as AnyObject)}) else {
+            return
+        }
+        delegateWrappers.remove(at: index)
+    }
+
+    public func invokeDelegates(_ closure: (ProtocolType) -> Void) {
+        delegates.forEach {closure($0)}
+    }
+}
+
+
+public protocol EmergencyResponding {
+    func notifyFire(at location: String)
+    func notifyCarCrash(at location: String)
+}
+
+public class FireStation: EmergencyResponding {
+    public func notifyFire(at location: String) {
+        print("FireFighters are notify fire at \(location)")
+    }
+
+    public func notifyCarCrash(at location: String) {
+        print("FireFighters are notify about car crash at \(location)")
+    }
+}
+
+public class PoliceStation: EmergencyResponding {
+
+    public func notifyFire(at location: String) {
+        print("Police notify about the Fire at \(location)")
+    }
+
+    public func notifyCarCrash(at location: String) {
+        print("Police notify about the car carsh at \(location)")
+    }
+}
+
+
+public class DispatchSystem {
+    let muticastDelegate = MulticastDelegate<EmergencyResponding>()
+}
+
+let dispatchSystem = DispatchSystem()
+var policeStation: PoliceStation! = PoliceStation()
+var fireStation: FireStation! = FireStation()
+
+dispatchSystem.muticastDelegate.addDelegate(policeStation)
+dispatchSystem.muticastDelegate.addDelegate(fireStation)
+
+dispatchSystem.muticastDelegate.invokeDelegates { delegate in
+    delegate.notifyFire(at: "Main Market!")
+}
+
+
+fireStation = nil
+
+dispatchSystem.muticastDelegate.invokeDelegates { (delegate) in
+    delegate.notifyCarCrash(at: "Highway Road!")
+}
